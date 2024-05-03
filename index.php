37d628cdd095ff69499a91c90ec3753e769cc3b8
Iago Alves <iagoads@outlook.com>
2024-05-03 09:15:06 -0300

    att

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Em Breve</title>
+</head>
+<body>
+    <h1>
+        Em breve estará online
+    </h1>
+    <a href="./admin">Admin</a>
+</body>
+</html> 
